a052f6fea79d474a93b1fa2f9e448baf075bfc5d
Přemysl Pleva <premyslpleva75@gmail.com>
2024-08-21 19:25:17 +0200

    Project 2 - v 2.0
    Significant blocks of the code moved into functions, simplified branching of if/elif/else, reduced nesting of loops

--- Bulls_and_Cows.py
+++ Bulls_and_Cows.py
@@ -9,74 +9,43 @@ import random as r
 from datetime import datetime
 import math
 
-print(
-    "Hi there!",
-    "-" * 50,
-    "I've generated a random 4 digit number for you.",
-    "Let's play a bulls and cows game.",
-    "-" * 50,
-    "Enter a number:",
-    "-" * 50,
-    sep="\n"
-)
 
-
-# Následuje uživatelská funkce <bull_number> na generování náhodného 4-místného čísla bez opakování číslic
-#
 def bull_number():
     """
-    Funkce generuje náhodné 4-ciferné číslo, v němž se neopakuje žádná číslice
+    This function is generating a random 4-digit number
     """
-    # číslo pvní generuju jako string, a až na závěr jej převedu do integer
-    #
-    num_str = ""
+
+    num_list = []
     digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    #
-    # Ve smyčve vždy vyberu jeden prvek z <digits> a připojím jej na konec <num_str>
-    # Následně vybraný prvek odstraním z množiny <digits> (aby nedošlo k opakování číslic)
-    #
-    for i in range(5):
-        #
-        #
+
+    for i in range(4):
         a = r.choice(digits)
-        #
-        # Ověřím, zda není první číslice "nula" - proto se jedná o 5-ti kolečkový cyklus
-        #
-        if a == "0" and len(num_str) == 0:
-            continue
-        #
-        # Připojím vybranou číslici ke stringu
-        #
-        num_str += a
-        #
-        # Odstraním vybranou číslici z <digits>
-        #
+        num_list += a
         digits.remove(a)
-        #
-        # Ověřím délku doteď vytvořeného čísla
-        #
-        if len(num_str) == 4:
-            break
-    #
-    # Převedu vytvořené číslo na integer
-    #
-    num = int(num_str)
+    while num_list[0] == "0":  # checking the first digit - it cannot be a zero
+        r.shuffle(num_list)
+
+    num = ''.join(num_list)
     return num
 
 
-# Následuje uživatelská funkce <total_time>, která počítá, jak dlouho hadač hádal
-#
 def total_time(a, z):
+    """
+    This function calculates the total time from a to z
+    :param a: start
+    :param z: end
+    :return: total time from a to z
+    """
     tot_sec = (z - a).total_seconds()
     if 119 < tot_sec < 180:
         m = math.floor(tot_sec / 60)
-        s = math.floor((tot_sec % 60) * 60)
-        a = f"It took you {m} minutes and {s} seconds :)"
+        s = math.floor((tot_sec % 60))
+        result = f"It took you {m} minutes and {s} seconds :)"
     elif tot_sec >= 180:
-        a = f"You are a lame and your game won't make it to the records..."
+        result = f"You are a lame and your game won't make it to the records..."
     else:
-        a = f" It took you {math.floor(tot_sec)} seconds only!"
-    return print(a)
+        result = f"It took you {math.floor(tot_sec)} seconds only!"
+    return print(result)
 
 
 def count_bulls_cows(tip):
@@ -89,102 +58,122 @@ def count_bulls_cows(tip):
             cows += 1
     return bulls, cows
 
+
+def game_conditions_ok(user_input: str) -> bool:
+    """
+    This function checks the game rules for player's input
+    :param user_input: Player's input, a number
+    :return: True or False
+    """
+    if not x.isnumeric():
+        print("Your input contains non-digit symbols...")
+        result = False
+    elif len(x) != 4:
+        print("Your input should be a 4-digit number...")
+        result = False
+    elif len(set(x)) != 4:
+        print("Digits do not repeat...")
+        result = False
+    else:
+        result = True
+    return result
+
+
+def wrong_guess():
+    """
+    This function prints the count of Bulls and Cows
+    :return: A string of counted Bulls and Cows
+    :example: 1 Bull, 0 Cows
+    """
+    if count_bulls_cows(x)[0] == 1:
+        b = "Bull"
+    else:
+        b = "Bulls"
+
+    if count_bulls_cows(x)[1] == 1:
+        c = "Cow"
+    else:
+        c = "Cows"
+
+    result = f"{count_bulls_cows(x)[0]} {b}, {count_bulls_cows(x)[1]} {c}"
+    return result
+
+
+def end_game_check():
+    '''
+    This function asks the player if they want to play another game with a newly generated number
+    :return: "y" for yes or "n" for no
+    '''
+    while True:  # Loop for asking if the player wants to play again
+        new_game = input("Would you like to play again, cowboy? (y/n):")
+        if new_game == "y":
+            print("*" * 50)
+            print("That's my boi! Let's GO!")
+            print("*" * 50)
+            break
+        elif new_game == "n":
+            print("*" * 50)
+            print("Thanks for playing, see you again soon.")
+            break
+        else:
+            continue
+
+    return new_game
+
+
 # THE GAME
 #
 #
-new_game = True
-#
-while new_game:
-    #
-    # generátor čísla, kt se má uhádnout
-    generated_num = str(bull_number())
-    # print(generated_num)
-    #
-    # počitadlo pokusů
-    i = 0
-    mistakes = True
-    #
-    # stopky - start
-    start = datetime.now()
-    #
-    while mistakes:
-        #
-        # uživatelův tip
-        #
-        x = input()
-        #
-        # Kontrola podmínek hry
-        #
-        if not x.isnumeric():
-            print("Zadal jsi nepovolené znaky! Znovu...")
+print(
+    "Hi there!",
+    "-" * 50,
+    "I've generated a random 4 digit number for you.",
+    "It has unique digits.",
+    "Let's play Bulls and Cows game.",
+    "-" * 50,
+    "Enter a number:",
+    "-" * 50,
+    sep="\n"
+)
+
+
+while True:  # Loop for the entire game
+
+    generated_num = bull_number()  # The random number that is to be guessed
+    print(generated_num)
+
+    guesses = 0  # trial & error counter
+    # again = str()  # An empty string - used later to check in the player wants a new game
+
+    start = datetime.now()  # timer - start
+
+    while True:  # Loop where the player is guessing the number
+
+        x = input()  # player's guess
+        guesses += 1
+
+        if game_conditions_ok(x) is False:  # Verifying validity of player's input
             continue
-        elif len(x) > 4:
-            print("Zadal jsi příliš dlouhé číslo! Znovu...")
+
+        if count_bulls_cows(x)[0] < 4:  # Wrong guess, less than 4 Bulls
+            print(wrong_guess())
             continue
-        elif len(x) < 4:
-            print("Zadal jsi příliš krátké číslo! Znovu...")
-            continue
-        elif len(set(x)) != 4:
-            print("Čislice se nesmí opakovat! Znovu...")
-            continue
-        else:
-            #
-            # případ, kdy uživatel úhádne správné číslo
-            #
-            if count_bulls_cows(x)[0] == 4:
-                i += 1
-                #
-                # stopky - konec
-                end = datetime.now()
-                print("*" * 50)
-                print("Correct!! Amazing game, congratulations :)", f"You got it in {i} turns!", sep="\n")
-                total_time(start, end)
-                print("*" * 50)
-                #
-                # následuje větvení, zda-li chce uživatel hrát znovu
-                while True:
-                    again = input("Would you like to play again, cowboy? (y/n):")
-                    if again not in ("y", "n"):
-                        continue
-                    elif again == "n":
-                        mistakes = False
-                        new_game = False
-                        print("*" * 50)
-                        print("Thanks for playing, see you again soon.")
-                        break
-                    elif again == "y":
-                        mistakes = False
-                        print("*" * 50)
-                        print("That's my boi! Let's GO!")
-                        print("*" * 50)
-                        break
-            #
-            #
-            # špatný tip, pouze 1x bull a 1x cow
-            elif count_bulls_cows(x)[0] == 1 and count_bulls_cows(x)[1] == 1:
-                i += 1
-                print(f"{count_bulls_cows(x)[0]} Bull, {count_bulls_cows(x)[1]} Cow")
-                print("-" * 50)
-                continue
-            #
-            # špatný tip, pouze 1x bull
-            elif count_bulls_cows(x)[0] == 1 and count_bulls_cows(x)[0] != 1:
-                i += 1
-                print(f"{count_bulls_cows(x)[0]} Bull, {count_bulls_cows(x)[1]} Cows")
-                print("-" * 50)
-                continue
-            #
-            # špatný tip, pouze 1x cow
-            elif count_bulls_cows(x)[0] != 1 and count_bulls_cows(x)[1] == 1:
-                i += 1
-                print(f"{count_bulls_cows(x)[0]} Bulls, {count_bulls_cows(x)[1]} Cow")
-                print("-" * 50)
-                continue
-            #
-            # špatný tip, více bulls a cows
+
+        if count_bulls_cows(x)[0] == 4:  # Correct guess, 4 Bulls
+            end = datetime.now()  # timer - end
+            print("*" * 50)
+            if guesses == 1:
+                print("Wow! You must be a fortuneteller.")
             else:
-                i += 1
-                print(f"{count_bulls_cows(x)[0]} Bulls, {count_bulls_cows(x)[1]} Cows")
-                print("-" * 50)
-                continue
+                print("Correct!! Amazing game, congratulations :)", f"You got it in {guesses} turns!", sep="\n")
+            total_time(start, end)
+            print("*" * 50)
+
+        break
+
+    again = end_game_check()
+    if again == "n":
+        break
+
+
 
